c3ced3b370e5b70cbd5a7d83b01e0e9a83f5aed7
Han-Na-05-22 <gkssktkfkd5@naver.com>
2022-02-08 15:31:07 +0900

    react youtube

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -3,45 +3,71 @@ import axios from 'axios';
 
 export default function Gallery(){
     let main = useRef(null);
-    let [items, setItems] = useState([]);
+    const [items, setItems] = useState([]);
+    const [isPop, setIsPop] = useState(false);
+    const [index, setIndex] = useState(0);
     const api_key = '8fa271bedd4a017590060c13ebf89615';
     const method1 = "flickr.interestingness.getList";
-    const num = 5;
-    const username = "194808976@N04";
-    const url = `https://www.flickr.com/services/rest/?method=${method1}&per_page=${num}&api_key=${api_key}&format=json&nojsoncallback=1&user_id=${username}`;
+    const num = 20;
+    const url = `https://www.flickr.com/services/rest/?method=${method1}&per_page=${num}&api_key=${api_key}&format=json&nojsoncallback=1`;
 
 
     useEffect(()=>{
       main.current.classList.add("on");
       
       axios.get(url).then(json=>{
-            console.log(json.data.photos.photo);
             setItems(json.data.photos.photo);
         });
   },[]);
 
     return(
-        <main className="content gallery" ref={main}>
-           <figure></figure>
-            <div className="inner">
-                <h1>Gallery</h1>
-                <section>
-                    {items.map((data,idx)=>{
-                        let imgSrc = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`
+        <>
+            <main className="content gallery" ref={main}>
+            <figure></figure>
+                <div className="inner">
+                    <h1>Gallery</h1>
+                    <section>
+                        {items.map((data,idx)=>{
+                            let imgSrc = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`
 
-                        return(
-                            <article key={idx}>
-                                <div className="inner">
-                                    <div className="pic">
-                                        <img src={imgSrc} />
+                            return(
+                                <article key={idx}>
+                                    <div className="inner">
+                                        <div className="pic" onClick={()=>{
+                                            setIsPop(true);
+                                            setIndex(idx);
+                                        }}>
+                                            <img src={imgSrc} />
+                                        </div>
+                                        <h2>{data.title}</h2>
                                     </div>
-                                    <h2>{data.title}</h2>
-                                </div>
-                            </article>
-                        )
-                    })}
-                </section>
-            </div>
-        </main>
+                                </article>
+                            )
+                        })}
+                    </section>
+                </div>
+            </main>
+            {isPop ? <Popup /> : null}
+        </>
     );
+
+    function Popup(){
+        useEffect(()=>{
+            document.body.style.overflow="hidden";
+
+            return()=>{
+                document.body.style.overflow="auto";
+            }
+        },[])
+        return(
+            <aside className="popup">
+                <h2>{items[index].title}</h2>
+                <img src={`https://live.staticflickr.com/${items[index].server}/${items[index].id}_${items[index].secret}_b.jpg`} />
+                <span onClick={()=>{
+                    setIsPop(false);
+                }}>close</span>
+            </aside>
+        )
+    }
 }
+
--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,22 +1,77 @@
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef,useState} from 'react';
+import axios from 'axios';
 
 export default function Youtube(){
     let main = useRef(null);
+    const[items,setItems] = useState([]);
+    const [isPop,setIsPop] = useState(false);
+    const [index,setIndex] = useState(0);
+    const playListId = "PL0sD6ZGYpbrKWrPFqyeaTvHFb5lL8h4p7";
+    const Key = "AIzaSyCn1tyD9aK47ZQNCljr9-sDVPjoLxpdJhc";
+    const num = 10;
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${Key}&playlistId=${playListId}&maxResults=${num}`;
+    
 
     useEffect(()=>{
         main.current.classList.add("on");
 
-        return()=>{
-      }
+        axios.get(url).then(json=>{
+            console.log(json.data.items);
+            setItems(json.data.items);
+        })
   },[])
-
+// 리턴문 안에서는 조건문은 삼항연산자, 반복은 map 만 동작이 됨
     return(
-        <main className="content youtube" ref={main}>
-            <figure></figure>
-            <div className="inner">
-                <h1>Youtube</h1>
-                <section></section>
-            </div>
-        </main>
+        <> {/*프래그먼트로 묶어줌*/}
+            <main className="content youtube" ref={main}>
+                <figure></figure>
+                <div className="inner">
+                    <h1>Youtube</h1>
+                    <section>
+                        {items.map((item,idx)=>{
+                            let tit = item.snippet.title;
+                            let tit_len = tit.length;
+
+                            let desc = item.snippet.description;
+                            let desc_len = desc.length;
+                            return(
+                                <article key={idx}>
+                                    <div className="inner">
+                                        <div className="txt">
+                                            <h2>{tit_len > 40 ? tit.substr(0,40)+'...' : tit}</h2>
+                                            <p>{desc_len > 150 ? desc.substr(0,150)+'...' : desc}</p>
+                                        </div>
+                                        <div className="pic" onClick={()=>{
+                                        setIsPop(true);
+                                        setIndex(idx);
+                                        }}>
+                                            <img src={item.snippet.thumbnails.standard.url} alt="" />
+                                        </div>
+                                    </div>
+                                </article>
+                            )
+                        })}
+                    </section>
+                </div>
+            </main>
+            {isPop ? <Pop /> : null}
+        </>
     );
+
+    function Pop(){
+        useEffect(()=>{
+            document.body.style.overflow="hidden";
+            return()=>{
+                document.body.style.overflow="auto";
+            }
+        })
+        return(
+            <aside className="pop">
+                <iframe src={`https://www.youtube.com/embed/${items[index].snippet.resourceId.videoId}&frameborder=0`}></iframe>
+                <span onClick={()=>{
+                    setIsPop(false);
+                }}>close</span>
+            </aside>
+        )
+    }
 }
